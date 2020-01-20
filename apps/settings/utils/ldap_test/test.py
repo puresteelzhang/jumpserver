f6dec6157625503da76f3a1d5ea630be929fc6f3
Bai <bugatti_it@163.com>
2020-01-20 11:07:46 +0800

    [Update] 添加LDAPTest工具 2

--- apps/settings/utils/ldap_test/test.py
+++ apps/settings/utils/ldap_test/test.py
@@ -9,7 +9,7 @@ from django.conf import settings
 
 class LDAPConfigTest(object):
     """
-    1. 测试LDAPServer、Port可以连接性
+    1. 测试LDAPServer、Port可以连接性 - OK
     2. 测试管理账号DN用户名、密码正确性
     3. 测试获取回来的用户信息的属性映射字段
     4. 测试用户过滤器配置是否正确
@@ -33,7 +33,32 @@ class LDAPConfigTest(object):
         return config
 
     def test_server_uri(self):
-        pass
+        import socket
+        server_uri = self.config.get('server_uri', '')
+        start = server_uri.find('//')
+        if start == -1:
+            print('Error: Server uri does not contain ldap:// or ldaps://')
+            return False
+        end = server_uri.rfind(':')
+        if end == -1:
+            print('Error: Server uri does not contain port')
+            return False
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            host = server_uri[start + 2:end]
+            port = int(server_uri[end+1:])
+            s.connect((host, port))
+        except ConnectionRefusedError as e:
+            print('Error: The server uri is unconnectable: {}'.format(e))
+            return False
+        except Exception as e:
+            print('Error: {}'.format(e))
+            return False
+        else:
+            print('Success: Test server uri are connectable')
+            return True
+        finally:
+            s.close()
 
     def test_bind_dn(self):
         pass
